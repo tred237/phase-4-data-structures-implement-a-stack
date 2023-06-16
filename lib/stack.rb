a6f296a1d78aac95322c86c5683d2483d5793666
trevor dewindt <trevor.dewindt@gmail.com>
2023-06-16 12:17:30 -0600

    commit tree 752344c484154a0c88d958469e0caf59c07654d6

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,40 @@
-# your code here
+class Stack
+    attr_accessor :stack, :limit
+    
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        if limit && @stack.length >= limit 
+            raise "Stack Overflow"
+        else 
+            @stack.push(value)
+        end
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[-1]
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        limit ? @stack.length == limit : false
+    end
+
+    def search(value)
+        @stack.index(value) ? @stack.size - @stack.index(value) - 1 : -1
+    end
+end
